d874bb6353803adcf48773890386814e2ef38707
Suvichan Prasongphonchai <acspdoctoraod@gmail.com>
2017-09-11 15:58:28 +0700

    Add code

--- src/server.js
+++ src/server.js
@@ -1,2 +1,22 @@
 import moment from 'moment'
-console.log(moment().format('YYYY-MM-DD HH:MM:DD'))
+import mongoose from 'mongoose'
+console.log('Start: ', moment().format('YYYY-MM-DD HH:MM:DD'))
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/test', {useMongoClient: true});
+
+var User = mongoose.model('User', { name: String });
+
+var children = new User({ name: 'Aod' });
+// children.save(function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('meow');
+//   }
+// });
+
+const getUser = async () => {
+  const response = await User.find({name: 'Aod'})
+  console.log(response)
+}
+getUser()
